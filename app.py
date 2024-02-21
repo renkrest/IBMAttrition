76ecfa0db099013a04c06a98ac2d5f1150340dc1
renkrest <134942239+renkrest@users.noreply.github.com>
2024-02-21 13:53:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,43 +43,43 @@ else:
     with col1:
         Age = st.number_input('Age', value=25)
     with col2:
-        BusinessTravel = st.selectbox('Business Travel', ['Non_Travel', 'Travel_Rarely', 'Travel_Frequently'])     
+        BusinessTravel = st.selectbox('BusinessTravel', ['Non_Travel', 'Travel_Rarely', 'Travel_Frequently'])     
    # Baris Kedua
     with st.container():
         col1, col2 = st.columns(2)
     with col1:
-        DistanceFromHome = st.number_input('Distance From Home', value=5)
+        DistanceFromHome = st.number_input('DistanceFromHome', value=5)
     with col2:
-        Education = st.selectbox('Education', ['Life Sciences', 'Medical', 'Human Resources', 'Marketing','Other', 'Technical Degree'])
+        Education = st.selectbox('Education', [1, 2, 3, 4, 5])
 
    # Baris Ketiga
     with st.container():
        col1, col2, col3 = st.columns(3)
     with col1:
-       JobSatisfaction = st.selectbox('Satisfaction', [1, 2, 3,4])
+       JobSatisfaction = st.selectbox('JobSatisfaction', [1, 2, 3,4])
     with col2:
-       JobInvolvement = st.selectbox('Involvement', [1, 2, 3,4])        
+       JobInvolvement = st.selectbox('JobInvolvement', [1, 2, 3,4])        
     with col3:
        JobLevel = st.selectbox('Level',[1, 2, 3,4,5])        
    # Baris Keempat
     with st.container():
         col1, col2, col3 = st.columns(3)
     with col1:
-        MaritalStatus = st.selectbox('Marital Status',['Divorced', 'Married', 'Singles'])    
+        MaritalStatus = st.selectbox('MaritalStatus',['Divorced', 'Married', 'Single'])    
     with col2:
-        MonthlyIncome = st.number_input('Monthly Income', value=10000.0)
+        MonthlyIncome = st.number_input('MonthlyIncome', value=10000.0)
     with col3:
-        EducationField = st.selectbox('Education Field',[1,2,3,4,5,6])
+        EducationField = st.selectbox('EducationField',['Life Sciences', 'Medical', 'Human Resources', 'Marketing','Other', 'Technical Degree'])
        
    # Baris Kelima
     with st.container():
         col1, col2, col3 = st.columns(3)
     with col1:
-        TotalWorkingYears = st.number_input('Total Working Years', value=5)
+        TotalWorkingYears = st.number_input('TotalWorkingYears', value=5)
     with col2:
-        YearsAtCompany = st.number_input('Years At Company', value=5) 
+        YearsAtCompany = st.number_input('YearsAtCompany', value=5) 
     with col3:
-        OverTime = st.selectbox('Over Time',[1, 0])
+        OverTime = st.selectbox('OverTime',[1, 0])
 
    # Inference
     data = {
@@ -90,12 +90,12 @@ else:
            'Satisfaction': JobSatisfaction,
            'Involvement': JobInvolvement,
            'Level': JobLevel,
-           'Marital Status': MaritalStatus, 
-           'Monthly Income': MonthlyIncome,
-           'Education Field': EducationField,
-           'Total Working Years': TotalWorkingYears,
-           'Years At Company': YearsAtCompany,
-           'Over Time': OverTime
+           'MaritalStatus': MaritalStatus, 
+           'MonthlyIncome': MonthlyIncome,
+           'EducationField': EducationField,
+           'TotalWorkingYears': TotalWorkingYears,
+           'YearsAtCompany': YearsAtCompany,
+           'OverTime': OverTime
            }
 
    # Tabel data
