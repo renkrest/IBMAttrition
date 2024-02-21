14ccffc4bfa29b7261f084a5ce08ba2592e86752
renkrest <134942239+renkrest@users.noreply.github.com>
2024-02-21 16:43:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ else:
            'TotalWorkingYears': TotalWorkingYears,
            'YearsAtCompany': YearsAtCompany,
            'OverTime': OverTime,
-           'DailyRate': DailyRate
+           'DailyRate': DailyRate,
            'EmployeeCount': EmployeeCount,
            'EnvironmentSatisfaction': EnvironmentSatisfaction,
            'HourlyRate': HourlyRate,
